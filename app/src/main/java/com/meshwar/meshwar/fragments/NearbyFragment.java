26e850aada09231247ccf4ce66093f0e0497b3cf
Nawal <Nawalbadarneh18@gmail.com>
2024-01-13 21:18:22 +0300

    activate nearby places

--- app/src/main/java/com/meshwar/meshwar/fragments/NearbyFragment.java
+++ app/src/main/java/com/meshwar/meshwar/fragments/NearbyFragment.java
@@ -65,7 +65,7 @@ public class NearbyFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity() , NearbyMapActivity.class);
-                intent.putExtra("KEY" ,"university");
+                intent.putExtra("KEY" ,"car_repair");
                 startActivity(intent);
             }
         });
